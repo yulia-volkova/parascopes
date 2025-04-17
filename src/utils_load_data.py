c440a969ba19bd88cb0b9813d2a2ff9f91ff55b3
Nicky Pochinkov <work@nicky.pro>
2025-04-17 14:38:41 +0000

    can now load single layers and choose operations

--- src/utils_load_data.py
+++ src/utils_load_data.py
@@ -9,15 +9,43 @@ DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 torch.set_grad_enabled(False)
 folder = BASE_DIR
 
-def load_res_data(index, group_size=4, groups_to_load=2):
+def load_res_data_internal(index, group_size=2, group_operation="cat"):
     file_path = f"{folder}/res_tensors/res_data_{index:03d}.pt"
     data = torch.load(file_path, map_location="cpu", weights_only=False)
     data = torch.cat(data, dim=2)  # Concatenate list of tensors
     data = data.squeeze(0)
-    data = data[1:, :, :]  # Remove first layer
     data = einops.rearrange(data, 'layers samples dim -> samples layers dim')
-    data = einops.rearrange(data, 'samples (layersg g) dim -> samples layersg (g dim)', g=group_size)
+    first_layer = data[:, 0:1, :]
+    data = data[:, 1:, :]  # Remove first layer
+
+    # Get cumulative activations data
+    if "cum" in group_operation:
+        data[:, 0, :] = data[:, 0, :] + first_layer.squeeze(1)
+        data = torch.cumsum(data, dim=1)
+
+    # split into groups
+    data = einops.rearrange(data, 'samples (layersg g) dim -> samples layersg g dim', g=group_size)
+
+    # apply operation to groups
+    if group_operation == "sum":
+        data = einops.reduce(data, 'samples layers g dim -> samples layers dim', reduction="sum")
+        group_size = 1
+    elif group_operation in ["cat", "cumcat"]:
+        data = einops.rearrange(data, 'samples layers g dim -> samples layers (g dim)', g=group_size)
+    else:
+        raise ValueError(f"Unknown group operation: {group_operation}")
+
+    # return for final processing
+    return data
+
+def load_res_data(index, group_size=2, groups_to_load=0, group_operation="cat"):
+    data = load_res_data_internal(index, group_size, group_operation)
     data = einops.rearrange(data[:, -groups_to_load:], 'samples layersg gdim -> samples (layersg gdim)')
+    return data.float()
+
+def load_res_data_layer(index, layer_idx, group_size=2, group_operation="cat"):
+    data = load_res_data_internal(index, group_size, group_operation)
+    data = data[:, layer_idx, :]
     return data.float()
 
 def load_embeds(index):
@@ -33,6 +61,12 @@ def flatten(nested_list):
 
 def load_split_paragraphs(index):
     file_path = f"{folder}/split_paragraphs/paragraphs_{index:03d}.json"
+    with open(file_path, 'r') as f:
+        data = json.load(f)
+    return data
+
+def load_split_paragraphs_tokenized(index):
+    file_path = f"{folder}/split_paragraphs/paragraphs_tokenized_sonar_{index:03d}.json"
     with open(file_path, 'r') as f:
         data = json.load(f)
     return data
